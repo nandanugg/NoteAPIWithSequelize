d2065230dbebe472bc83588ac776d7189b8d83b1
nandanugg <navy.techno@gmail.com>
2021-02-17 02:31:43 +0700

    Add missing fixes

--- route/notesRoute.js
+++ route/notesRoute.js
@@ -26,7 +26,7 @@ app.put('/:id', async (req, res, next) => {
 
 app.delete('/:id', async (req, res, next) => {
   const { id } = req.params
-  await remove({ id }).catch(next)
+  await remove(id).catch(next)
   res.send("ok")
 })
 
